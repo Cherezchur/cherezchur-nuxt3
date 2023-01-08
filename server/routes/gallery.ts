cb7129dff1e61a1d3964626d2b60740bc3ce7179
TarasovAndrej <tarasov.a@rusrobots.ru>
2023-01-08 16:50:00 +0300

    create gallery data

---
+++ server/routes/gallery.ts
@@ -0,0 +1,17 @@
+export default defineEventHandler(() => {
+  return {
+    gallery: [
+      {
+        title: "Обложка к альбому 'Ivanovo City drug drift", 
+        description: "В центре комопозиции находится надпись 'Distorshn Li and Harly Hantom 2'. Изначально работа подразумевалась как обложка к совместному релизу двух исполнителей - Cherezchur и Livename. Для них были придуманы шуточные псевдонимы: 'Distorshn Li' и 'Harly Hantom'. Но в итоге Livename отказался участвовать в релизе. Альбом вышел сольно, но обложка сохранилась, как память о времени рождения и релиза и как инструменет передачи атмосферы треков",
+        images: [
+          "distorshn-li-and-harly-hantom.png",
+          "distorshn-li-and-harly-hantom-2.jpg",
+          "distorshn-li-and-harly-hantom-3.jpg",
+          "distorshn-li-and-harly-hantom-4.jpg",
+        ],
+        isLikes: false,
+      },
+    ]
+  }
+})
