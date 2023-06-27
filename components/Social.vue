52e75871e1538f2e0b77d9b21152d911a004f019
TarasovAndrej <tarasov.a@rusrobots.ru>
2023-06-28 01:10:54 +0300

    create social

---
+++ components/Social.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+	<ul class="social">
+		<li class="social__item">
+			<NuxtLink :to="'#'" class="social__link" />
+		</li>
+	</ul>
+</template>
+
+<style lang="scss">
+
+</style>
